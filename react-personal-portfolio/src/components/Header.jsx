09d950a23ead0970539710f35aea32ee6db49318
Diyari <dearyfareq15@gmail.com>
2023-02-05 14:18:53 +0300

    added navbar items

--- react-personal-portfolio/src/components/Header.jsx
+++ react-personal-portfolio/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-
+import css from './Header.module.scss'
+import {BiPhoneCall} from 'react-icons/bi'
 
 const Header = () => {
     return(
-        <div>hi there</div>
+        //main wrapper
+        <div className={`paddings ${css.wrapper}`}>
+            {/* items container with flex and auto margin */}
+            <div className={`flexCenter innerWidth ${css.container}`}>
+              <div className={css.name}>
+                  Diyari
+              </div>
+              {/* list items with phone number and icon from react */}
+              <ul>
+                <li><a href="">services</a></li>
+                <li><a href="">experince</a></li>
+                <li><a href="">portfolio</a></li>
+                <li><a href="">testimonials</a></li>
+                <li>
+                    <p>0751-191-3938</p>
+                    <BiPhoneCall size ={"40px"} />
+                </li>
+              </ul>
+            </div>
+        </div>
     )
 }
 
