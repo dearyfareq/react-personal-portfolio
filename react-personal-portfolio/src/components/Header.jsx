db7cd63d0a0eef15b4ac2dcfcc0ea8c9ed17cbeb
Diyari <dearyfareq15@gmail.com>
2023-02-05 14:49:02 +0300

    added NavBar SCSS

--- react-personal-portfolio/src/components/Header.jsx
+++ react-personal-portfolio/src/components/Header.jsx
@@ -7,17 +7,18 @@ const Header = () => {
         //main wrapper
         <div className={`paddings ${css.wrapper}`}>
             {/* items container with flex and auto margin */}
-            <div className={`flexCenter innerWidth ${css.container}`}>
+            <div className={`innerWidth ${css.container}`}>
               <div className={css.name}>
-                  Diyari
+                  DIYARI
               </div>
               {/* list items with phone number and icon from react */}
-              <ul>
+              
+              <ul className={` flexCenter ${css.list}`}>
                 <li><a href="">services</a></li>
                 <li><a href="">experince</a></li>
                 <li><a href="">portfolio</a></li>
                 <li><a href="">testimonials</a></li>
-                <li>
+                <li className={css.phone}>
                     <p>0751-191-3938</p>
                     <BiPhoneCall size ={"40px"} />
                 </li>
