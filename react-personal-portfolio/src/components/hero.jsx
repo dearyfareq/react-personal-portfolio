67daed838b6705c49413c62941cb24c5b0e3a6fc
Diyari <dearyfareq15@gmail.com>
2023-02-07 15:24:25 +0300

    added left & right side hero animation

--- react-personal-portfolio/src/components/hero.jsx
+++ react-personal-portfolio/src/components/hero.jsx
@@ -2,54 +2,79 @@ import React from "react";
 import css from "../components/hero.module.scss";
 import person from "../../public/person.png";
 import logo from "../../public/certificate.png";
-import {motion} from "framer-motion";
-import { staggerContainer } from "../utils/motion";
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 //don't forget to import images before using them, if so; use {import name}
 const Hero = () => {
   return (
     <section className={` paddings ${css.wrapper}`}>
       <motion.div
-       initial="hidden"
-       whileInView="show"
-       variants={staggerContainer}
-       viewport={{once: false, amount: 0.25}}
-       className={`innerWidth ${css.container}`}
+        initial="hidden"
+        whileInView="show"
+        variants={staggerContainer}
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
       >
-          {/* upper hero elements */}
-          <div className={css.upper}>
-            <span className="primaryText">
-              Guten Tag!
-              <br /> I'm Diyari
-            </span>
-            <span className="secondaryText">
-              I make simple yet,
-              <br /> beautiful artwork
-            </span>
-          </div>
+        {/* upper hero elements */}
+        <div className={css.upper}>
+          <motion.span
+            className="primaryText"
+            variants={fadeIn("right", "tween", 0.2, 1)}
+          >
+            Guten Tag!
+            <br /> I'm Diyari
+          </motion.span>
+          <motion.span
+            className="secondaryText"
+            variants={fadeIn("left", "tween", 0.2, 1)}
+          >
+            I make simple yet,
+            <br /> beautiful artwork
+          </motion.span>
+        </div>
 
-          {/* center image of the person*/}
-          <div className={css.person}>
-            <img src={person} alt="person" />
-          </div>
+        {/* center image of the person*/}
+        <div className={css.person}>
+          <img src={person} alt="person" />
+        </div>
 
-          {/* lower hero elements */}
-          <div className={css.lower}>
-            <div className={css.skills}>
-              <div className="primaryText">+3</div>
-              <div className="secondaryText">
-                <p>
-                  Years
-                  <br />
-                  Designing
-                </p>
-              </div>
-            </div>
-            <div className={css.freelance}>
-              <img src={logo} alt="Icon" />
-              <span className="secondaryText">Advance Graphic</span>
-              <span className="secondaryText">UI/UX Designer</span>
+        {/* lower hero elements */}
+        <div className={css.lower}>
+          <div className={css.skills}>
+            <motion.div
+              className="primaryText"
+              variants={fadeIn("right", "tween", 0.2, 1)}
+            >
+              +3
+            </motion.div>
+            <div className="secondaryText">
+              <motion.p variants={fadeIn("right", "tween", 0.2, 1)}>
+                Years
+                <br />
+                Designing
+              </motion.p>
             </div>
           </div>
+          <div className={css.freelance}>
+            <motion.img 
+            src={logo} 
+            alt="Icon" 
+            variants={fadeIn("left","tween",0.2,1)}
+            />
+            <motion.span
+              className="secondaryText"
+              variants={fadeIn("left", "tween", 0.2, 1)}
+            >
+              Advance Graphic
+            </motion.span>
+            <motion.span
+              className="secondaryText"
+              variants={fadeIn("left", "tween", 0.2, 1)}
+            >
+              UI/UX Designer
+            </motion.span>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
