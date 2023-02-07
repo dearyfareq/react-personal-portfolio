0897417fea4ce5949263a3caf8309d7d39be9fc6
Diyari <dearyfareq15@gmail.com>
2023-02-07 13:23:47 +0300

    added hero section elements with comments to separate code
    three main containers, upper-person-lower

---
+++ react-personal-portfolio/src/components/hero.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import css from "../components/hero.module.scss";
+import person from "../../public/person.png";
+import logo from "../../public/certificate.png";
+
+//don't forget to import images before using them, if so; use {import name}
+const Hero = () => {
+  return (
+    <section className={` paddings ${css.wrapper}`}>
+      <div className={` innerWidth ${css.container}`}>
+
+        {/* upper hero elements */}
+        <div className={css.upper}>
+          <span className="primaryText">
+            Guten Tag!
+            <br /> I'm Diyari
+          </span>
+          <span className="secondaryText">
+            I make simple yet,
+            <br /> beautiful artwork
+          </span>
+        </div>
+
+        {/* center image of the person*/}
+        <div className={css.person}>
+            <img src={person} alt="person" />
+        </div>
+
+        {/* lower hero elements */}
+        <div className={css.lower}>
+            <div className={css.skills}>
+                <div className="primaryText">+3</div>
+                <div className="secondaryText">
+                    <p>Years<br/>Designing</p>
+                </div>
+            </div>
+            <div className={css.freelance}>
+                <img src={logo} alt="Icon" />
+                <span className="secondaryText">Advance Graphic</span>
+                <span className="secondaryText">UI/UX Designer</span>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
